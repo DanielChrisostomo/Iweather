ceb75ef13fe2bfbbf21766e751736af0de4cf40a
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-07-24 16:28:56 -0300

    Agrupando testes com describe

--- src/utils/getNextDay.test.ts
+++ src/utils/getNextDay.test.ts
@@ -1,9 +1,17 @@
 import { getNextDays } from './getNextDays';
 
-it("should be returned the next five days", () => {
-    const days = getNextDays();
-    console.log(days)
-
-    expect(days.length).toBe(5)
+describe("Grupo de testes | getNextDays", () => {
+    it("Teste 1 should be returned the next five days", () => {
+        const days = getNextDays();
+        console.log(days)
+    
+        expect(days.length).toBe(5)
+    })
+    
+    it("Teste 2 should be returned the next five days", () => {
+        const days = getNextDays();
+        console.log(days)
+    
+        expect(days.length).toBe(5)
+    })
 })
-
