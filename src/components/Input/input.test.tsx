0c600a58975e071986ca91290eb95dc7b5e0901a
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-07-25 09:33:23 -0300

    toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -2,10 +2,17 @@ import { render, screen } from "@testing-library/react-native";
 import { Input } from "@components/Input"
 
 describe("Component: Input", () => {
-    it("should  be render without activity indicator if isLoading prop is undefined.", () => {
-        render( <Input isLoading/> )
+    it("should be rendered without activity indicator if isLoading prop is undefined.", () => {
+        render( <Input /> )
+
+        const activityIndicator = screen.queryByTestId('activity-indicator')
+        expect(activityIndicator).toBeNull()
+    })
+
+    it("should be rendered without activity indicator if isLoading prop is true.", () => {
+        render( <Input isLoading /> )
 
         const activityIndicator = screen.getByTestId('activity-indicator')
-        console.log(activityIndicator)
+        expect(activityIndicator).toBeTruthy()
     })
 })
