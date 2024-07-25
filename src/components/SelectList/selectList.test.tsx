fb0a4552b80ff1de5ae5c930679e44b0f77e93ca
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-07-25 10:57:44 -0300

    Exemplo de testes com mocks

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -1,5 +1,5 @@
+import { render, screen, fireEvent } from "@testing-library/react-native"
 import { SelectList } from "@components/SelectList"
-import { render, screen } from "@testing-library/react-native"
 
 describe("Component: SelectList", () => {
     it("Should be returned city details selected.", () => {
@@ -19,6 +19,23 @@ describe("Component: SelectList", () => {
         const selectedCity = screen.getByText("Campinas")
     })
 
+    it("Should be returned city details selected.", () => {
+        const data = [
+            { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+            { id: '2', name: 'Campo grande', latitude: 124, longitude: 456 }
+        ]
+
+        render( 
+        <SelectList 
+            data={data}  
+            onChange={() => {}}
+            onPress={() => {}}
+            />
+        )
+
+        const selectedCity = screen.getByText(/campo/i)
+    })
+
     it("Should not be returned city details selected.", () => {
         const data = [
             { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
@@ -35,5 +52,49 @@ describe("Component: SelectList", () => {
 
         const selectedCity = screen.queryByText("Florianópolis")
         expect(selectedCity).toBeFalsy()
+    })
+
+    it("Should be called the function once with the city details selected", () => {
+        const data = [
+            { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+            { id: '2', name: 'Campo grande', latitude: 124, longitude: 456 }
+        ]
+
+        const onPress = jest.fn()
+
+        render( 
+        <SelectList 
+            data={data}  
+            onChange={() => {}}
+            onPress={onPress}
+            />
+        )
+
+        const selectedCity = screen.getByText(/campo/i)
+        fireEvent.press(selectedCity)
+
+        expect(onPress).toBeCalledTimes(1);
+    })
+
+    it("Should be called the function once with the city details selected", () => {
+        const data = [
+            { id: '1', name: 'Campinas', latitude: 123, longitude: 456 },
+            { id: '2', name: 'Campo grande', latitude: 124, longitude: 456 }
+        ]
+
+        const onPress = jest.fn()
+
+        render( 
+        <SelectList 
+            data={data}  
+            onChange={() => {}}
+            onPress={onPress}
+            />
+        )
+
+        const selectedCity = screen.getByText(/campo/i)
+        fireEvent.press(selectedCity)
+
+        expect(onPress).toBeCalledWith(data[1])
     })
 })
